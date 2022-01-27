030250e9e47cbcf4708351f01577e9d6d2fed620
AyushUprety <ayushuprety1@gmail.com>
2022-01-27 19:18:19 +0545

    google auth was setupped using react-google-login

--- front_end/src/components/login.jsx
+++ front_end/src/components/login.jsx
@@ -1,11 +1,34 @@
-import React from "react"
-import { FcGoogle } from 'react-icons/fc';
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { GoogleLogin } from "react-google-login";
+import Logo from "../assets/logo.png";
 
-const Login = ()=>{
-    return(
-        <div>
-            <FcGoogle/>
-        </div>
-    )
-}
+const Login = () => {
+    const responseGoogle = (response) => {
+        console.log(response);
+    }
+  return (
+    <div>
+      <img alt="images" src={Logo} width="130px" />
+      <div>
+        <GoogleLogin
+          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          render={(renderProps) => (
+            <button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              <FcGoogle/>sign in with google
+            </button>
+          )}
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+        
+      </div>
+    </div>
+  );
+};
 export default Login;
