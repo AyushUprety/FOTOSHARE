b493eb783fbebcced1ef70639764d3d7ecf2c18c
AyushUprety <ayushuprety1@gmail.com>
2022-01-27 19:34:42 +0545

    added video and brand to the app using tailwind css

--- front_end/src/components/login.jsx
+++ front_end/src/components/login.jsx
@@ -2,32 +2,38 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { GoogleLogin } from "react-google-login";
 import Logo from "../assets/logo.png";
+import appVideo from "../assets/share.mp4";
 
 const Login = () => {
-    const responseGoogle = (response) => {
-        console.log(response);
-    }
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
   return (
     <div>
-      <img alt="images" src={Logo} width="130px" />
-      <div>
-        <GoogleLogin
-          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-          render={(renderProps) => (
-            <button
-              onClick={renderProps.onClick}
-              disabled={renderProps.disabled}
-            >
-              <FcGoogle/>sign in with google
-            </button>
-          )}
-          buttonText="Login"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={"single_host_origin"}
-        />
-        
-      </div>
+      <header>
+        <video autoPlay loop muted>
+          <source src={appVideo} type="video/mp4" />
+        </video>
+        <img alt="images" src={Logo} width="130px" />
+        <div>
+          <GoogleLogin
+            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                <FcGoogle />
+                sign in with google
+              </button>
+            )}
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
+      </header>
     </div>
   );
 };
