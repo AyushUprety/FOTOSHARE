95d5e5f0d3308995912927bdc519a8e2fd5b3ee4
AyushUprety <ayushuprety1@gmail.com>
2022-01-27 13:50:01 +0545

    Added containers that store large part of project and component store individual pieces of logic

--- front_end/src/app.js
+++ front_end/src/app.js
@@ -1,14 +1,12 @@
 import React from "react";
-import {Routes,Route,userNavigate} from 'react-router-dom'
+import { Routes, Route, userNavigate } from "react-router-dom";
 
-const App = ()=>{
-    return(
-        <Routes>
-            <Route path='/login' element={<login/>}/>
-            <Route path='/*' element={<home/>} />
-        </Routes>
-           
-        
-    )
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<login />} />
+      <Route path="/*" element={<home />} />
+    </Routes>
+  );
+};
 export default App;
