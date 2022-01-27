97d57e3a821689ba77402633f33d3d6fddbbe095
AyushUprety <ayushuprety1@gmail.com>
2022-01-27 13:47:56 +0545

    Added a route for login and all the other pages

--- front_end/src/app.js
+++ front_end/src/app.js
@@ -1,10 +1,14 @@
 import React from "react";
+import {Routes,Route,userNavigate} from 'react-router-dom'
 
 const App = ()=>{
     return(
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <Routes>
+            <Route path='/login' element={<login/>}/>
+            <Route path='/*' element={<home/>} />
+        </Routes>
+           
+        
     )
 }
 export default App;
