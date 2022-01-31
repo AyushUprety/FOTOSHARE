28a6e30c67cc6c53ca5bd23b2bf45e2a2354616b
AyushUprety <ayushuprety1@gmail.com>
2022-01-31 13:54:26 +0545

    Added flex properties to align hamburger and brand logo

--- front_end/src/container/home.jsx
+++ front_end/src/container/home.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link, Route, Routes } from 'react-router-dom';
+import logo from '../assets/logowhite.png';
 
 
 const Home = ()=>{
     return (
     <div className='bg-red-100 flex flex-row'>
-      <div className="md:display-none flex flex-col">
-
+      <div className="hidden md:flex flex-row">
+        <img src={logo} width="130px"/>
+      </div>
+      <div className="md:hidden w-screen h-screen flex flex-row justify-between">
+        <HiMenu/>
+        <Link to='/'>
+          <img src={logo} width="130px"/>
+        </Link>
+        
       </div>
     </div>
       )
