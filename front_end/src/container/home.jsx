5fa4226b2c0c5eef2f8e590f8b2ac06a2f8fa27e
AyushUprety <ayushuprety1@gmail.com>
2022-01-31 13:20:54 +0545

    Added tailwind in home.jsxto improve styling

--- front_end/src/container/home.jsx
+++ front_end/src/container/home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
 
 const Home = ()=>{
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+    return (
+    <div className='bg-red-100 flex flex-row'>
+      <div className="md:display-none flex flex-col">
+
+      </div>
+    </div>
+      )
 }
 export default Home;
