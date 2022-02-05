d8d3f85bfdf84713f59fbf08e3b1d177eedf85a4
AyushUprety <ayushuprety1@gmail.com>
2022-02-05 11:00:15 +0545

    All the elements of Categories array were map in sidebar and user was passed as a props

--- front_end/src/container/sidebar.jsx
+++ front_end/src/container/sidebar.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { RiHomeFill } from "react-icons/ri";
 
-export const sidebar = () => {
+export const sidebar = (props) => {
+  const{user}=props;
   const Categories = [
     "Cats",
     "Dogs",
@@ -19,13 +20,17 @@ export const sidebar = () => {
       <h1>FotoShare</h1>
       <div className="flex flex-row p-2">
         <Link to="/">
-          <RiHomeFill/>
+          <RiHomeFill />
           <p>Home</p>
         </Link>
       </div>
       <p>Discover Categories</p>
       {Categories.map((item) => {
-        return <div>{item}</div>;
+        return (
+          <NavLink to="/category/categoryType">
+            <div>{item}</div>
+          </NavLink>
+        );
       })}
     </div>
   );
