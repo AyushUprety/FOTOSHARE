09c9fb2c50d251373367ddbddad9a92b2ae3ea14
AyushUprety <ayushuprety1@gmail.com>
2022-02-05 20:42:56 +0545

    Added a basic design to navbar that contains search input form and plus icon

--- front_end/src/components/search.jsx
+++ front_end/src/components/search.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React from "react";
+import { IoMdAdd, IoMdSearch } from "react-icons/io";
 
-export const search = () => {
-  return <div>
-      Search
-  </div>;
+export const Search = () => {
+  return (
+    <div>
+      <form>
+        <IoMdSearch fontSize={21} className="ml-1" />
+        <input type="search" />
+      </form>
+      <IoMdAdd className="bg-black text-white" />
+    </div>
+  );
 };
 export default Search;
--- front_end/src/container/pin.jsx
+++ front_end/src/container/pin.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from '../components/navbar'
+import Search from '../components/search'
 
  const pin = () => {
   return (
     <div>
-      Pin
+      <Navbar/>
+      <Search/>
     </div>
   )
 };
