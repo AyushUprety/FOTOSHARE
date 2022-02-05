bfc9c5b7a15a8aa42a97eaa9a63725824f20e7f0
AyushUprety <ayushuprety1@gmail.com>
2022-02-05 20:29:15 +0545

    added new components search and navbar

---
+++ front_end/src/components/search.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const search = () => {
+  return <div>
+      Search
+  </div>;
+};
+export default Search;
--- front_end/src/container/sidebar.jsx
+++ front_end/src/container/sidebar.jsx
@@ -38,6 +38,7 @@ export const Sidebar = (props) => {
             onClick={()=>setToggle(true)}
           >
             {item}
+            {console.log(item)}
           </NavLink>
         );
       })}
