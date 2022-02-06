6d0cb245578e521c3f4ee1271aff95d1c33639ff
AyushUprety <ayushuprety1@gmail.com>
2022-02-06 11:43:55 +0545

    Added search bar search icon and plus icon

--- front_end/src/components/navbar.jsx
+++ front_end/src/components/navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
+import { IoMdAdd, IoMdSearch } from "react-icons/io";
 
 export const navbar = () => {
-  return <div>
-      navbar
-  </div>;
+  return (
+    <div className="flex flex-row w-full items-center p-3">
+      <form className="flex flex-row w-full h-full pr-2">
+        <IoMdSearch fontSize={21} className="ml-1 bg-white h-6" />
+
+        <input className="w-full" type="search" />
+      </form>
+      <IoMdAdd className="w-6 h-full bg-black text-white" />
+    </div>
+  );
 };
 export default navbar;
--- front_end/src/components/search.jsx
+++ front_end/src/components/search.jsx
@@ -3,13 +3,10 @@ import { IoMdAdd, IoMdSearch } from "react-icons/io";
 
 export const Search = () => {
   return (
-    <div>
-      <form>
-        <IoMdSearch fontSize={21} className="ml-1" />
-        <input type="search" />
-      </form>
-      <IoMdAdd className="bg-black text-white" />
-    </div>
-  );
+      <div>
+          
+      </div>
+  )
+    
 };
 export default Search;
